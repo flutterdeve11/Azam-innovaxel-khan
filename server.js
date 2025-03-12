5ea6d9941d39222643bbfd69967aad282126097a
Azamkhan <azamdeve939@gmail.com>
2025-03-12 14:43:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(express.json());
+
+// Database connection
+connectDB();
+
+// Routes
+app.use('/shorten', urlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
